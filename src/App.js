78a88547221952fb3b193fe79710f01b646cba27
SauloFilho <saulosilvaf@gmail.com>
2020-02-20 20:04:36 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [techs, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </>
+  );
+}
+
+export default App;
